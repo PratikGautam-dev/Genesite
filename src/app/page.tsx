a669c75a17a743670d919c92bcad02c9457344f7
PratikGBihar <gautampratik483@gmail.com>
2025-07-24 14:42:03 +0530

    ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const Page=()=>{
+  const [value,setvalue]=useState("")
   const trpc=useTRPC();
-  const invoke=useMutation(trpc.invoke.mutationOptions({}))
+  const invoke=useMutation(trpc.invoke.mutationOptions({
+    onSuccess:()=>{
+      toast.success("Background job started")
+    }
+  }))
   return(
     <div className="p-4 max-w-7xl mx-auto">
-      <Button onClick={()=>invoke.mutate({text:"John"})}>
+      <Input value={value}onChange={(e)=>setvalue(e.target.value)}/>
+      <Button onClick={()=>invoke.mutate({value:value})}>
         Invoke 
       </Button>
     </div>
